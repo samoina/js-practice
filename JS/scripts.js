511e8c0d52ad0b3cd7cc1e4a75dd74cb726075c7
Samoina <samoina.w@gmail.com>
2021-12-07 10:57:24 +0300

    Create HTML file, CSS, JS and image folders

---
+++ JS/scripts.js
@@ -0,0 +1,16 @@
+let today = new Date ();                // this is a new constructor for Date assigned to a variable called today
+let hourNow = today.getHours();         // a new variable that is assigned to get the time today and to display a greeting using if else
+let greeting;
+
+if (hourNow > 18) {
+  greeting = "Good Evening!";
+} else if (hourNow > 12) {
+  greeting = "Good Afternoon!";
+} else if (hourNow > 0) {
+  greeting = "Habari za asubuhi";
+} else {
+  greeting = "Karibu sana!";
+}
+
+document.write("<h3>" + greeting + "</h3>");
+
