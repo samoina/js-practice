ad5f32359087f94e910a25a3e9110349424107f8
Samoina <samoina.w@gmail.com>
2021-12-07 12:51:23 +0300

    Add comment to explain what the script does

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,7 @@
-let today = new Date ();                // this is a new constructor for Date assigned to a variable called today
+//This script displays a greeting to the user based on their current local time. It is an example from the JS and jQuery book
+
+
+let today = new Date ();                // this creates a new date object
 let hourNow = today.getHours();         // a new variable that is assigned to get the time today and to display a greeting using if else
 let greeting;
 
@@ -12,5 +15,7 @@ if (hourNow > 18) {
   greeting = "Karibu sana!";
 }
 
-document.write("<h3>" + greeting + "</h3>");
+document.write("<h3>" + greeting + "</h3>");      //if you write the script within the html file we will have document.write("Habari za asubui"); where document is the object, .write is the method that allows new content to be written onto the page while in () we have the data required for the method to work. Remember, when the browser comes across a script element, it stops to load the script and check if there is panything it needs to do. 
+
+//NB: the section in {} determines which code should run and represent blocks of code. new statements start on a new line and end with ;
 
